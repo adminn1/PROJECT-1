ce7ba34f58ae33c9eeffd8e97c3e8977c2abaf98
SoFluk <flukpaicomza@gmail.com>
2016-08-23 17:00:05 +0700

    เพิ่มรูปภาพ ดูวันสระผม

--- app/src/main/java/com/auspicious/auspicious/Page5Activity.java
+++ app/src/main/java/com/auspicious/auspicious/Page5Activity.java
@@ -3,6 +3,7 @@ package com.auspicious.auspicious;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Page5Activity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class Page5Activity extends AppCompatActivity {
 
         Intent getIntent = getIntent();
         String title = getIntent.getStringExtra("title");
+
+        ImageView xm5 = (ImageView) findViewById(R.id.xm5);
+        xm5.setImageResource(R.drawable.tx5);
 
 
         // view matching
