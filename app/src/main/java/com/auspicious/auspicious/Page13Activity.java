8ad9e2e114e76e04a3702edb2faff7095abde885
sofluk <flukpaicomza@gmail.com>
2016-08-24 15:25:09 +0700

    เพิ่มข้อมูลใน อัญมณี ปีเกิด

--- app/src/main/java/com/auspicious/auspicious/Page13Activity.java
+++ app/src/main/java/com/auspicious/auspicious/Page13Activity.java
@@ -3,6 +3,7 @@ package com.auspicious.auspicious;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Page13Activity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class Page13Activity extends AppCompatActivity {
 
         Intent getIntent = getIntent();
         String title = getIntent.getStringExtra("title");
+
+        ImageView xm13 = (ImageView) findViewById(R.id.xm13);
+        xm13.setImageResource(R.drawable.tx13);
 
 
         // view matching
