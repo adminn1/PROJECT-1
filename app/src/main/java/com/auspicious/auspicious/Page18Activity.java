027fc6c19b21439a09e3c4c091e3abbd94030c52
SoFluk <flukpaicomza@gmail.com>
2016-08-25 13:08:49 +0700

    เพิ่มข้อมูลลงใน กลางคืนได้ยินเสีบงร้องเรียกห้ามขานรับ

--- app/src/main/java/com/auspicious/auspicious/Page18Activity.java
+++ app/src/main/java/com/auspicious/auspicious/Page18Activity.java
@@ -3,6 +3,7 @@ package com.auspicious.auspicious;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Page18Activity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class Page18Activity extends AppCompatActivity {
 
         Intent getIntent = getIntent();
         String title = getIntent.getStringExtra("title");
+
+        ImageView xm18 = (ImageView) findViewById(R.id.xm18);
+        xm18.setImageResource(R.drawable.tx18);
 
 
         // view matching
