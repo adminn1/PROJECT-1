2ad8a7ea80465233a377459decddfc766836feaa
SoFluk <flukpaicomza@gmail.com>
2016-08-25 12:54:44 +0700

    เพิ่มข้อมูลใน จิ้งจกทัก

--- app/src/main/java/com/auspicious/auspicious/Page17Activity.java
+++ app/src/main/java/com/auspicious/auspicious/Page17Activity.java
@@ -3,6 +3,7 @@ package com.auspicious.auspicious;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Page17Activity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class Page17Activity extends AppCompatActivity {
 
         Intent getIntent = getIntent();
         String title = getIntent.getStringExtra("title");
+
+        ImageView xm17 = (ImageView) findViewById(R.id.xm17);
+        xm17.setImageResource(R.drawable.tx17);
 
 
         // view matching
