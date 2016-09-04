b483384532a25245a7cb941f0f18a0aada1aa96e
sofluk <flukpaicomza@gmail.com>
2016-09-04 23:44:23 +0700

    เอาข้อมูลลงสัตว์ป่าเข้าบ้าน

--- app/src/main/java/com/auspicious/auspicious/Page24Activity.java
+++ app/src/main/java/com/auspicious/auspicious/Page24Activity.java
@@ -3,6 +3,7 @@ package com.auspicious.auspicious;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Page24Activity extends AppCompatActivity {
@@ -18,6 +19,9 @@ public class Page24Activity extends AppCompatActivity {
 
         Intent getIntent = getIntent();
         String title = getIntent.getStringExtra("title");
+
+        ImageView xm24 = (ImageView) findViewById(R.id.xm24);
+        xm24.setImageResource(R.drawable.tx24);
 
 
         // view matching
